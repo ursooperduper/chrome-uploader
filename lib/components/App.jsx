0e61c497fea3288e23d432fb014ec1394a2b59b5
Sarah Kuehnle <sarah@sooperduper.com>
2015-02-06 19:46:34 -0500

    Adds a dropdown menu to the header.

--- lib/components/App.jsx
+++ lib/components/App.jsx
@@ -49,7 +49,7 @@ var App = React.createClass({
 
   render: function() {
     return (
-      <div className={'App App--' + this.state.page}>
+      <div className={'App App--' + this.state.page} onClick={this.hideMenuDropdown}>
         <div className="App-header">{this.renderHeader()}</div>
         <div className="App-page">{this.renderPage()}</div>
         <div className="App-footer">{this.renderFooter()}</div>
@@ -68,7 +68,7 @@ var App = React.createClass({
 
     return <LoggedInAs
         user={this.state.user}
-        onLogout={this.appActions.logout.bind(this.appActions)} />;
+        onLogout={this.appActions.logout.bind(this.appActions)} ref="dropdownMenu" />;
   },
 
   renderPage: function() {
@@ -140,6 +140,14 @@ var App = React.createClass({
         <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     );
+  },
+
+  hideMenuDropdown: function() {
+    var dropdownMenu = this.refs.dropdownMenu;
+
+    if (dropdownMenu) {
+      dropdownMenu.hideDropdown();
+    }
   }
 });
 
